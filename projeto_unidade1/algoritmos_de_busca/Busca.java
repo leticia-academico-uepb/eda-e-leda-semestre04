0044b23229ef681053a15c25e0290084a27d8104
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-09-30 20:09:39 -0300

    feat: busca binária iterativa para lista de Estudantes

--- projeto_unidade1/algoritmos_de_busca/Busca.java
+++ projeto_unidade1/algoritmos_de_busca/Busca.java
@@ -1,5 +1,7 @@
 package projeto_unidade1.algoritmos_de_busca;
 
+import projeto_unidade1.Estudante;
+
 public class Busca {
 
     public static int linearIterativa(Estudante[] vetor, int matriculaBuscada) {
@@ -8,6 +10,27 @@ public class Busca {
                 return i;
             }
         }
+        return -1;
+    }
+
+    public static int binariaIterativa(Estudante[] vetor, int matriculaBuscada) {
+        int inicio = 0;
+        int fim = vetor.length - 1;
+
+        while (inicio <= fim) {
+            int meio = inicio + (fim - inicio) / 2;
+
+            if (vetor[meio].getMatricula() == matriculaBuscada) {
+                return meio;
+            }
+
+            if (vetor[meio].getMatricula() < matriculaBuscada) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+
         return -1;
     }
 
